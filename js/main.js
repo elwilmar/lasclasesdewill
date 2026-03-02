64046dad4e42d13e0e0dfaad064f9e752253a3a5
Wilmar Pineda <wpineda.gonzalez@gmail.com>
2026-03-02 09:26:23 -0500

    Fix rutas producción GitHub Pages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-async function loadComponent(id, file) {
-    try {
-        const response = await fetch(file);
-        const content = await response.text();
-        document.getElementById(id).innerHTML = content;
-    } catch (error) {
-        console.error("Error cargando componente:", error);
+function getBasePath() {
+    const path = window.location.pathname;
+    const repoName = "lasclasesdewill";
+
+    if (path.includes(repoName)) {
+        return "/" + repoName;
+    } else {
+        return "";
     }
+}
+
+async function loadComponent(id, file) {
+    const base = getBasePath();
+    const response = await fetch(base + file);
+    const content = await response.text();
+    document.getElementById(id).innerHTML = content;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
