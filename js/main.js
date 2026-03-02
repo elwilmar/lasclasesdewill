08d4867060f039197935b40d0d700388c8495fc0
Wilmar Pineda <wpineda.gonzalez@gmail.com>
2026-03-02 09:15:05 -0500

    Actualización estructura por áreas + animaciones + header corregido

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("Sistema Web 2026 cargado correctamente.");
+async function loadComponent(id, file) {
+    try {
+        const response = await fetch(file);
+        const content = await response.text();
+        document.getElementById(id).innerHTML = content;
+    } catch (error) {
+        console.error("Error cargando componente:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadComponent("header", "/components/header.html");
+    loadComponent("footer", "/components/footer.html");
+});
